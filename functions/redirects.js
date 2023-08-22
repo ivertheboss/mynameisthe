e9e16899de23926cd736857b3a30ad0592b7d85f
TheColdLemoande <62616414+ivertheboss@users.noreply.github.com>
2023-08-22 16:30:45 -0400

    Update redirects.js

--- functions/redirects.js
+++ functions/redirects.js
@@ -1,11 +1,11 @@
-//exports.handler = async function(event, context) {
-//  const redirectUrl = 'http://69.14.211.78:8000'; // The external IP and port
-//
-//  return {
-//    statusCode: 302, // Redirect status code
-//    headers: {
-//      Location: redirectUrl
-//    },
-//    body: 'Redirecting...'
-//  };
-//};
+exports.handler = async function(event, context) {
+  const redirectUrl = 'https://mynameisthe.xyz/'; // The external IP and port
+
+  return {
+    statusCode: 302, // Redirect status code
+    headers: {
+      Location: redirectUrl
+    },
+    body: 'Redirecting...'
+  };
+};
